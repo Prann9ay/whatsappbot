9084c5961448832750c814212c1b4b610b967d14
root-byte <53040133+root-byte@users.noreply.github.com>
2021-10-06 04:34:55 +0530

    Add files via upload

---
+++ functions/list.js
@@ -0,0 +1,64 @@
+module.exports.commandList = (prefix) => {
+  return `*─「 🔥 <{PVX}> BOT 🔥 」─*
+  
+📛 *${prefix}sticker*
+    - _Create original size sticker from different media types!_
+  
+📛 *${prefix}sticker crop*
+    - _Create full size sticker from different media types!_
+
+📛 *${prefix}add <phone number>*
+    - _Add any new member!
+    
+📛 *${prefix}kick <mention>*
+    - _Kick any member from group!
+    - Alias: !ban , !remove 
+
+📛 *${prefix}mute | ${prefix}unmute*
+    - _Mute and Unmute the group!_
+
+📛 *${prefix}block*
+    - _To block particular commands for this group!_
+    
+📛 *${prefix}insta url*
+    - _Instagram post videos / reels downloader!_
+    
+📛 *${prefix}song name*
+    - _Get songs in good quality!_
+
+📛 *${prefix}text*
+    - _Get text extractor from image!_
+
+📛 *${prefix}quote*
+    - _Give a random quote!_
+
+📛 *${prefix}gender firstname*
+    - _Get gender from person first name!_
+      
+📛 *${prefix}technews*
+    - _Get latest Tech news from inshorts !_
+  
+📛 *${prefix}source*
+    - _Get bot source code!_
+
+📛 *${prefix}dev*
+    - _Get dev contact to report bug or to add new feature!_
+
+📛 *${prefix}pvxlink*
+    - _Get links for all PVX groups!_
+
+_*🏏  IPL COMMANDS:*_
+NOTE: Put matchID (from cricbuzz) in description starting!
+  
+📛 *${prefix}score*
+    - _Give IPL match current score!_
+📛 *${prefix}startipl*
+    - _Start IPL match live score every 1 min!_
+📛 *${prefix}stopipl*
+    - _Stop IPL match live score!_
+      
+ ✔️ more cool commands coming...`;
+};
+
+// 📛 *${prefix}drive query_name*
+//     - _Get direct links of files from PVX team drive!_
